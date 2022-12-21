69d7a56c0ebe05c3e8a894dd5055125a4f1e4006
Laisa <laisacristina2606@gmail.com>
2022-12-21 15:42:13 -0300

    Entrega

--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,73 @@
-/* Monte a lógica das requisições aqui */
+// /* Monte a lógica das requisições aqui */
+
+async function consomePokeAPI() {
+  
+  const loading = document.querySelector('#loading')
+  
+  const pokemonsDaAPI = await fetch('https://pokeapi.co/api/v2/pokemon')
+  .then(
+      
+    res => res.json()
+    
+  )
+  .catch(
+      
+    error => console.log(error)
+
+  )
+  
+  loading.classList.add('hidden')
+  
+  return pokemonsDaAPI
+}
+
+async function getPokemonByName(pokemonName){
+
+  const pokemon =  await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`, {
+
+    method: 'GET',
+    headers: {
+      'Content-Type' : 'application/json'
+    }
+  })
+
+  .then(res => res.json())
+
+  .then(res =>{
+
+    return res
+  })
+
+  return pokemon
+
+}
+
+function renderSearch(){
+
+  const searchInput = document.querySelector('input')
+  const searchBtn = document.querySelector('#searchBtn')
+
+    searchBtn.addEventListener('click', async () =>{
+
+    renderPesq(await getPokemonByName(searchInput.value))
+
+  })
+
+}
+
+async function renderPesq(resposta){
+
+  const ulTag = document.querySelector('ul')
+  ulTag.innerHTML = ''
+  ulTag.insertAdjacentHTML('beforeend', `
+    <li class="lista">
+      <img class="imgPersonagem" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${resposta.id}.png" alt=${resposta.name}>
+      <h3 class="nomePersonagem">${resposta.name}</h3>
+    </li>
+
+  `)
+
+}
+
+renderSearch()
+consomePokeAPI()
